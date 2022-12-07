774d68b05b2ae520427f7ee80776ee946b2ff14e
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-12-07 21:20:45 +0100

    TEST - Challenge 5

---
+++ chalenges/5-challenge/5-challenge.test.ts
@@ -0,0 +1,20 @@
+import { getMaxGifts } from "./5-challenge";
+
+describe("Max gifts", () => {
+  it("test - 1", () => {
+    const giftsCities = [12, 3, 11, 5, 7];
+    const maxGifts = 20;
+    const maxCities = 3;
+
+    expect(getMaxGifts(giftsCities, maxGifts, maxCities)).toBe(20); // 20
+  });
+  test("", () => {
+    expect(getMaxGifts([12, 3, 11, 5, 7], 20, 3)).toBe(20); // 20
+    expect(getMaxGifts([50], 15, 1)).toBe(0); // 0
+    expect(getMaxGifts([50], 100, 1)).toBe(50); // 50
+    expect(getMaxGifts([50, 70], 100, 1)).toBe(70); // 70
+    getMaxGifts([50, 70, 30], 100, 2); // 100
+    getMaxGifts([50, 70, 30], 100, 3); // 100
+    getMaxGifts([50, 70, 30], 100, 4); // 100
+  });
+});
