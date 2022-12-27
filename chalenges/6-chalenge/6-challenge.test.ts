19c868b816c421fa7879bc9c8ad1041918231dd5
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-12-27 20:21:51 +0100

    TEST - should create a 3 cube

--- chalenges/6-chalenge/6-challenge.test.ts
+++ chalenges/6-chalenge/6-challenge.test.ts
@@ -1,7 +1,7 @@
 import { createCube } from "./6-challenge";
 
 describe("createCube", () => {
-  it("should create a cube", () => {
+  it("should create a 3 cube", () => {
     const cube = createCube(3);
     const expected =
       "  /\\_\\_\\_\\\n /\\/\\_\\_\\_\\\n/\\/\\/\\_\\_\\_\\\n\\/\\/\\/_/_/_/\n \\/\\/_/_/_/\n  \\/_/_/_/";
