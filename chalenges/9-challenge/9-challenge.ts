9a4b95d407471346db04634217c1fc246b09527e
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-12-29 17:21:16 +0100

    ADD - countTime

--- chalenges/9-challenge/9-challenge.ts
+++ chalenges/9-challenge/9-challenge.ts
@@ -1,3 +1,23 @@
 export function countTime(leds: number[]) {
-  return 0;
+  let steps = 0;
+  let ledsAux = leds;
+  const length = ledsAux.length;
+  let onLeds = ledsAux.reduce((accumulator, led) => accumulator + led, 0);
+
+  while (onLeds < length) {
+    ledsAux = ledsAux.map((led, index) => {
+      const prevLed = index == 0 ? ledsAux[length - 1] : ledsAux[index - 1];
+
+      if (prevLed == 1) return 1;
+      if (led == 1) return 1;
+      return 0;
+    });
+    steps++;
+    onLeds = ledsAux.reduce((accumulator, led) => accumulator + led, 0);
+    console.log("onLeds3", onLeds);
+    console.log("ledsAux", ledsAux);
+    console.log("steps", steps);
+  }
+
+  return steps * 7;
 }
